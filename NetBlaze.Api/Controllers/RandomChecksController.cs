ffb57741c057094151fcc9eccb4e77804d17b3d8
Ritaj Zamel <90522032+Ritaj19Zamel@users.noreply.github.com>
2025-12-08 07:00:55 -0800

    Add route for OTP generation and verification

--- NetBlaze.Api/Controllers/RandomChecksController.cs
+++ NetBlaze.Api/Controllers/RandomChecksController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using NetBlaze.Application.Interfaces.ServicesInterfaces;
 using NetBlaze.SharedKernel.Dtos.OTP.Requests;
 using NetBlaze.SharedKernel.HelperUtilities.General;
@@ -14,11 +14,12 @@ namespace NetBlaze.Api.Controllers
             _randomChecksService = randomChecksService;
         }
 
-        [HttpPost()]
+        [HttpPost("generateotp")]
         public async Task<ApiResponse<object>> GenerateOTP(GenerateOtpRequestDto generateOtpRequestDto, CancellationToken cancellationToken = default)
         {
             return await _randomChecksService.GenerateOTP(generateOtpRequestDto, cancellationToken);
         }
+        [HttpPost("verifyotp")]
 
         public async Task<ApiResponse<object>> VerifyOTP(VerifyOTPRequestDto verifyOTPRequestDto, CancellationToken cancellationToken = default)
         {
